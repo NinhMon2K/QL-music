1e2181fdf97bc4ba44255449e5200554d3b8670b
Ninh Nguyễn Nghĩa <nguyennghianinh98@gmail.com>
2022-10-04 20:42:34 +0700

    4/10

--- app/src/views/album/MsAlbum.vue
+++ app/src/views/album/MsAlbum.vue
@@ -1,11 +1,51 @@
 <template>
-  <div>Album</div>
+  <div class="container-albums">
+    
+
+    <div class="container-content">
+
+      <div class="container-content__left">
+
+        <div class="product-album">
+          <ul class="list-album" >
+            <li class="item-album" v-for="(itemAlbum,index) in allData" :key="index">
+              <div class="app-icon ic-albums"></div>
+              <p>{{itemAlbum.name}}</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="container-content__right">
+
+      </div>
+
+    </div>
+  </div>
 </template>
 <script>
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "MsAlbum",
-  setup() {},
+  setup() {
+
+    const allData = [
+      {
+        name:'asdasd',
+
+      },
+       {
+        name:'asdasd',
+
+      },
+      
+    ];
+    return {
+      allData
+
+    }
+  },
 });
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
